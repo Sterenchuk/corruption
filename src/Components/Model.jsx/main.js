44d039bae27e17cf8c449081387734d427da9c22
Sterenchuk <osterenchuk@gmail.com>
2024-11-26 23:12:49 +0200

    FE refactored for use state

--- src/Components/Model.jsx/main.js
+++ src/Components/Model.jsx/main.js
@@ -2,31 +2,31 @@
 // eslint-disable-next-line
 import React, { Component } from "react";
 
-class Validate extends Component {
-  constructor(
-    rang,
-    salary,
-    bribeSize,
-    companyTransparencyLevel = 1,
-    companyCoraptionLevel = 1,
-    faith = 1
-  ) {
-    super();
-    this.rang = rang;
-    this.salary = salary;
-    this.bribeSize = bribeSize;
-    this.companyTransparencyLevel = companyTransparencyLevel;
-    this.companyCoraptionLevel = companyCoraptionLevel;
-    this.faith = faith;
-    this.validateData();
-  }
+// class Validate extends Component {
+//   constructor(
+//     rang,
+//     salary,
+//     bribeSize,
+//     companyTransparencyLevel = 1,
+//     companyCoraptionLevel = 1,
+//     faith = 1
+//   ) {
+//     super();
+//     this.rang = rang;
+//     this.salary = salary;
+//     this.bribeSize = bribeSize;
+//     this.companyTransparencyLevel = companyTransparencyLevel;
+//     this.companyCoraptionLevel = companyCoraptionLevel;
+//     this.faith = faith;
+//     this.validateData();
+//   }
 
-  validateData() {}
-}
+//   validateData() {}
+// }
 //this remove warning from console about useless react, but this component use React
 // eslint-disable-next-line
 
-export default class Coraption extends Validate {
+export default class Coraption extends Component {
   constructor(
     rang,
     salary,
@@ -35,15 +35,18 @@ export default class Coraption extends Validate {
     companyCoraptionLevel,
     faith
   ) {
-    super(
-      rang,
-      salary,
-      bribeSize,
-      companyTransparencyLevel,
-      companyCoraptionLevel,
-      faith
-    );
-    this.calculateCoraption();
+    this.state = {
+      rang: 2,
+      salary: 4000,
+      bribeSize: 20000,
+      companyTransparencyLevel: 2,
+      companyCoraptionLevel: 5,
+      faith: 2,
+      result: 0,
+      resutArray: []
+    };
+
+    this.calculateCoraption() = this.calculateCoraption.bind(this);
   }
 
   amplificate(...args) {
@@ -60,157 +63,157 @@ export default class Coraption extends Validate {
 
   //bribe
   A1() {
-    return this.bribeSize >= 1000 && this.bribeSize <= 5000
+    return this.state.bribeSize >= 1000 && this.state.bribeSize <= 5000
       ? 1
-      : 10 / 9 - this.bribeSize / 45000;
+      : 10 / 9 - this.state.bribeSize / 45000;
   }
 
   A2() {
-    if (this.bribeSize >= 1000 && this.bribeSize < 15000) {
-      return this.bribeSize / 14000 - 1 / 14;
-    } else if (this.bribeSize >= 15000 && this.bribeSize <= 25000) {
+    if (this.state.bribeSize >= 1000 && this.state.bribeSize < 15000) {
+      return this.state.bribeSize / 14000 - 1 / 14;
+    } else if (this.state.bribeSize >= 15000 && this.state.bribeSize <= 25000) {
       return 1;
     } else {
-      return -this.bribeSize / 25000 + 2;
+      return -this.state.bribeSize / 25000 + 2;
     }
   }
 
   A3() {
-    return this.bribeSize >= 35000 && this.bribeSize <= 50000
+    return this.state.bribeSize >= 35000 && this.state.bribeSize <= 50000
       ? 1
-      : this.bribeSize / 35000 + 1 / 35;
+      : this.state.bribeSize / 35000 + 1 / 35;
   }
 
   //rang
   B1() {
-    return this.rang >= 1 && this.rang <= 3
+    return this.state.rang >= 1 && this.state.rang <= 3
       ? 1
-      : Math.pow(-this.rang / 7 + 10 / 7, 2);
+      : Math.pow(-this.state.rang / 7 + 10 / 7, 2);
   }
 
   B2() {
-    if (this.rang >= 1 && this.rang < 5) {
-      return Math.pow(this.rang / 4 - 1 / 4, 2);
-    } else if (this.rang >= 5 && this.rang <= 7) {
+    if (this.state.rang >= 1 && this.state.rang < 5) {
+      return Math.pow(this.state.rang / 4 - 1 / 4, 2);
+    } else if (this.state.rang >= 5 && this.state.rang <= 7) {
       return 1;
     } else {
-      return Math.pow(-this.rang / 3 + 10 / 3, 2);
+      return Math.pow(-this.state.rang / 3 + 10 / 3, 2);
     }
   }
 
   B3() {
-    return this.rang >= 8 && this.rang <= 10 ? 1 : this.rang / 7 - 1 / 7;
+    return this.state.rang >= 8 && this.state.rang <= 10 ? 1 : this.state.rang / 7 - 1 / 7;
   }
   //salary
   C1() {
-    return this.salary >= 3000 && this.salary <= 5000
+    return this.state.salary >= 3000 && this.state.salary <= 5000
       ? 1
-      : (1 / 500) * Math.sqrt(-10 * this.salary + 300000);
+      : (1 / 500) * Math.sqrt(-10 * this.state.salary + 300000);
   }
 
   C2() {
-    if (this.salary >= 3000 && this.salary <= 8000) {
-      return 0.01 * Math.sqrt(2 * this.salary - 6000);
-    } else if (this.salary >= 8000 && this.salary <= 15000) {
+    if (this.state.salary >= 3000 && this.state.salary <= 8000) {
+      return 0.01 * Math.sqrt(2 * this.state.salary - 6000);
+    } else if (this.state.salary >= 8000 && this.state.salary <= 15000) {
       return 1;
     } else {
-      return Math.sqrt(-6 * this.salary + 180000) / 300;
+      return Math.sqrt(-6 * this.state.salary + 180000) / 300;
     }
   }
 
   C3() {
-    return this.salary >= 20000 && this.salary <= 30000
+    return this.state.salary >= 20000 && this.state.salary <= 30000
       ? 1
-      : (1 / 1700) * Math.sqrt(170 * this.salary - 510000);
+      : (1 / 1700) * Math.sqrt(170 * this.state.salary - 510000);
   }
 
   //corruption lvl
   F1() {
-    if (this.faith >= 1 && this.faith <= 3) {
+    if (this.state.faith >= 1 && this.state.faith <= 3) {
       return 1;
     } else {
-      return (-Math.pow(Math.abs(this.faith - 2), 1 / 3) + 2) / 2;
+      return (-Math.pow(Math.abs(this.state.faith - 2), 1 / 3) + 2) / 2;
     }
   }
 
   F2() {
-    if (this.faith >= 4 && this.faith <= 6) {
+    if (this.state.faith >= 4 && this.state.faith <= 6) {
       return 1;
-    } else if (this.faith >= 1 && this.faith < 4) {
+    } else if (this.state.faith >= 1 && this.state.faith < 4) {
       const a = 1 / (Math.pow(4, 1 / 3) - 1);
-      return a * Math.pow(this.faith, 1 / 3) - a;
+      return a * Math.pow(this.state.faith, 1 / 3) - a;
     } else {
       const scale = 1 / Math.pow(4, 1 / 3);
-      return -Math.pow(this.faith - 6, 1 / 3) * scale + 1;
+      return -Math.pow(this.state.faith - 6, 1 / 3) * scale + 1;
     }
   }
 
   F3() {
-    if (this.faith >= 8 && this.faith <= 10) {
+    if (this.state.faith >= 8 && this.state.faith <= 10) {
       return 1;
     } else {
       const scale = 1 / Math.pow(7, 1 / 3);
-      return -scale * Math.pow(Math.abs(this.faith - 8), 1 / 3) + 1;
+      return -scale * Math.pow(Math.abs(this.state.faith - 8), 1 / 3) + 1;
     }
   }
 
   //faith lvl
 
   K1() {
-    return this.companyCoraptionLevel >= 1 && this.companyCoraptionLevel <= 2
+    return this.state.companyCoraptionLevel >= 1 && this.state.companyCoraptionLevel <= 2
       ? 1
-      : -Math.pow(this.companyCoraptionLevel - 2, 2) / 64 + 1;
+      : -Math.pow(this.state.companyCoraptionLevel - 2, 2) / 64 + 1;
   }
 
   K2() {
-    if (this.companyCoraptionLevel >= 4 && this.companyCoraptionLevel <= 6) {
+    if (this.state.companyCoraptionLevel >= 4 && this.state.companyCoraptionLevel <= 6) {
       return 1;
     } else if (
-      this.companyCoraptionLevel >= 1 &&
-      this.companyCoraptionLevel < 4
+      this.state.companyCoraptionLevel >= 1 &&
+      this.state.companyCoraptionLevel < 4
     ) {
-      return -Math.pow(this.companyCoraptionLevel - 4, 2) / 9 + 1;
+      return -Math.pow(this.state.companyCoraptionLevel - 4, 2) / 9 + 1;
     } else {
-      return -Math.pow(this.companyCoraptionLevel - 6, 2) / 16 + 1;
+      return -Math.pow(this.state.companyCoraptionLevel - 6, 2) / 16 + 1;
     }
   }
 
   K3() {
-    return this.companyCoraptionLevel >= 8 && this.companyCoraptionLevel <= 10
+    return this.state.companyCoraptionLevel >= 8 && this.state.companyCoraptionLevel <= 10
       ? 1
-      : -Math.pow(this.companyCoraptionLevel - 6, 2) / 49 + 1;
+      : -Math.pow(this.state.companyCoraptionLevel - 6, 2) / 49 + 1;
   }
 
   //company transparency
 
   T1() {
-    return this.companyTransparencyLevel >= 1 &&
-      this.companyTransparencyLevel <= 2
+    return this.state.companyTransparencyLevel >= 1 &&
+      this.state.companyTransparencyLevel <= 2
       ? 1
-      : -(this.companyTransparencyLevel - 2) / 8 + 1;
+      : -(this.state.companyTransparencyLevel - 2) / 8 + 1;
   }
 
   T2() {
     if (
-      this.companyTransparencyLevel >= 4 &&
-      this.companyTransparencyLevel <= 6
+      this.state.companyTransparencyLevel >= 4 &&
+      this.state.companyTransparencyLevel <= 6
     ) {
       return 1;
     } else if (
-      this.companyTransparencyLevel >= 1 &&
-      this.companyTransparencyLevel < 4
+      this.state.companyTransparencyLevel >= 1 &&
+      this.state.companyTransparencyLevel < 4
     ) {
-      return (this.companyTransparencyLevel - 4) / 3 + 1;
+      return (this.state.companyTransparencyLevel - 4) / 3 + 1;
     } else {
-      return -(this.companyTransparencyLevel - 6) / 4 + 1;
+      return -(this.state.companyTransparencyLevel - 6) / 4 + 1;
     }
   }
 
   T3() {
-    return this.companyTransparencyLevel >= 8 &&
-      this.companyTransparencyLevel <= 10
+    return this.state.companyTransparencyLevel >= 8 &&
+      this.state.companyTransparencyLevel <= 10
       ? 1
-      : (this.companyTransparencyLevel - 8) / 8 + 1;
+      : (this.state.companyTransparencyLevel - 8) / 8 + 1;
   }
 
   //chance to bribe
@@ -269,17 +272,17 @@ export default class Coraption extends Validate {
       const p4 = this.P4(i);
       const p5 = this.P5(i);
       const p6 = this.P6(i);
-      const p7 = this.P7(i);
+      //const p7 = this.P7(i);
 
-      resultArray.push(this.disjunction(p1, p2, p3, p4, p5, p6, p7));
+      resultArray.push(this.disjunction(p1, p2, p3, p4, p5, p6));
     }
     const result = this.findFirstMaximum(resultArray);
 
     //console.log(resultArray);
-    console.log("\n\n", result);
-
-    return result;
+    //console.log("\n\n", result);
+    this.setState({resultArray: resultArray});
+    this.setState({ result: result });
   }
 }
 
-const corp = new Coraption(2, 4000, 30000, 1, 1, 8);
+
